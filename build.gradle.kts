44a190be4e671475ca477d2946551c48fb17c1d2
KoecengDaun <Laurentius.farel@gmail.com>
2025-02-24 13:27:47 +0700

    Perubahan COde analyst

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -11,6 +12,13 @@ version = "0.0.1-SNAPSHOT"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ayamTerbang")
+        property("sonar.projectName", "Eshop-Prolan")
     }
 }
 
